b5138a87fe7193f59832906af9be5f52869a369c
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2023-12-13 15:44:27 +0100

    ADD check if both side is available and send email if not

--- src/views/emails/checkers/not-both-side-available.blade.php
+++ src/views/emails/checkers/not-both-side-available.blade.php
@@ -3,15 +3,32 @@
     <tr>
         <th>Porsche order number</th>
         <th>Porsche SEQ</th>
-        <th>Available Pillar</th>
+        <th>Pillar</th>
+        <th>Not found side</th>
     </tr>
     </thead>
     <tbody>
     @foreach($sequences as $sequence)
+        @php
+            $side = \AWF\Extension\Models\AWF_SEQUENCE::where([
+                ['SEPONR', '=', $sequence->SEPONR],
+                ['SEPSEQ', '=', $sequence->SEPSEQ],
+                ['SEPSEQ', '=', $sequence->SEPONR]
+                ])->first();
+
+            if ($side->SESIDE === 'L') {
+                $side = 'R';
+            }
+
+            if ($side->SESIDE === 'R') {
+                $side = 'L';
+            }
+        @endphp
         <tr>
             <td>{{ $sequence->SEPONR }}</td>
             <td>{{ $sequence->SEPSEQ }}</td>
             <td>{{ $sequence->SEPILL }}</td>
+            <td>{{ $side }}</td>
         </tr>
     @endforeach
     </tbody>
