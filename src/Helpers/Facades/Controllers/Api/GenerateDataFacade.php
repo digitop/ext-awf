538b2af45369fae4335606b7ec354ead4f7243ff
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-01-03 09:46:49 +0100

    ADD generate date make order

--- src/Helpers/Facades/Controllers/Api/GenerateDataFacade.php
+++ src/Helpers/Facades/Controllers/Api/GenerateDataFacade.php
@@ -2,10 +2,16 @@
 
 namespace AWF\Extension\Helpers\Facades\Controllers\Api;
 
+use App\Http\Controllers\production\order\OrderController;
+use App\Http\Requests\production\order\InsertRequest;
+use App\Models\PARAMETERS;
+use App\Models\PRWFDATA;
 use AWF\Extension\Helpers\Checkers\SavedData;
 use AWF\Extension\Models\AWF_SEQUENCE;
 use AWF\Extension\Models\AWF_SEQUENCE_LOG;
 use AWF\Extension\Models\AWF_SEQUENCE_WORKCENTER;
+use Illuminate\Foundation\Http\FormRequest;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +23,7 @@ use Illuminate\Support\Facades\File;
 
 class GenerateDataFacade extends Facade
 {
-    public function create(Request|null $request = null, Model|string|null $model = null): JsonResponse|null
+    public function create(Request|FormRequest|null $request = null, Model|string|null $model = null): JsonResponse|null
     {
         try {
             $this->generatePath();
@@ -52,7 +58,6 @@ class GenerateDataFacade extends Facade
             $data = explode(';', $row);
 
             if (!empty($data) && !empty($data[0])) {
-                 $start = new \DateTime();
                 $year = substr($data[5], 0, 4);
                 $month = substr($data[5], 4, 2);
                 $day = substr($data[5], 6, 2);
@@ -83,6 +88,8 @@ class GenerateDataFacade extends Facade
                         'PRCODE' => $sequenceData->SEARNU,
                     ]);
                 }
+
+                $this->makeOrder($sequenceData);
             }
         }
 
@@ -115,5 +122,35 @@ class GenerateDataFacade extends Facade
         }
 
         AWF_SEQUENCE::where('SEINPR', '=', 0)->delete();
+    }
+
+    protected function makeOrder(Model $sequenceData): void
+    {
+        $workflow = PRWFDATA::where('PRCODE', $sequenceData->PRCODE)->first();
+
+        $orderController = new OrderController;
+        $orderDefaultNotification = PARAMETERS::find('ORDER_DEFAULT_NOTIFICATION');
+        $orderNotificationEnabled = PARAMETERS::find('ORDER_NOTIFICATION_ENABLED');
+
+        $orderRequest = new InsertRequest([
+            "PRCODE" => $sequenceData->PRCODE, //TERMÉK KÓD
+            "PRORCO" => null, //null mindig
+            "ORCODE" => $sequenceData->SEPONR . '_' . $sequenceData->SEPSEQ . '_' . $sequenceData->SEARNU, //Megrendelés kód
+            "PFIDEN" => $workflow->PFIDEN, //Termék alapértelmezett gyártási folyamata
+            "ORSTAT" => 1, // MINDIG 1
+            "ORQUAN" => 1, // MINDIG 1
+            "ORQYEN" => 1,
+            "ORRQEN" => 1,
+            "ORNOID" => $orderDefaultNotification->PAVALU ?? null, //PARAMTER tábla ORDER_DEFAULT_NOTIFICATION
+            "ORNOTC" => $orderNotificationEnabled->PAVALU ?? 0, //PARAMTER tábla ORDER_NOTIFICATION_ENABLED
+            "ORNOSC" => 60, // DEFAULT 60
+            "ORSCTY" => 2, // MINDIG 2, Gyártási terv típus 2 = kézi
+            "ORSCVA" => 1, // MINDIG 1, gyártási terv kezdő érték
+            "ORSCW1" => 5, // mindig 5, gyártási terv 1. figyelmeztetés
+            "ORSCW2" => 10, // mindig 10, gyártási terv 2. figyelmeztetés
+            "ORSCSW" => null, // null mindig, gyártási terv dátum alapú váltása
+        ]);
+
+        $orderController->store($orderRequest);
     }
 }
