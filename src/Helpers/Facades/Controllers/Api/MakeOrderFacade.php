9a10ad6da6d86154305b76f45c77f650d300e602
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-01-18 10:42:14 +0100

    FIX order make facade storage use

--- src/Helpers/Facades/Controllers/Api/MakeOrderFacade.php
+++ src/Helpers/Facades/Controllers/Api/MakeOrderFacade.php
@@ -14,6 +14,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class MakeOrderFacade extends Facade
 {
