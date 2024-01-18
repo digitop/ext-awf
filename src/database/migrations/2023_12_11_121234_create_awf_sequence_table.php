738d5eb9b3e63c1b452dfe3b4f9e2ef0d92d21b6
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-01-18 10:37:59 +0100

    FIX migrations

--- src/database/migrations/2023_12_11_121234_create_awf_sequence_table.php
+++ src/database/migrations/2023_12_11_121234_create_awf_sequence_table.php
@@ -18,12 +18,13 @@ return new class extends Migration {
             $table->id('SEQUID')->comment('AWF uniquely developed module sequence identifier');
             $table->string('SEPONR', 10)->comment('Porsche Order Number');
             $table->string('SEPSEQ', 10)->comment('Porsche Sequence Number');
-            $table->string('PRCODE', 32)->nullable()->comment('Product code (PRODUCT:PRCODE)');
+            $table->string('SEARNU', 15)->comment('Article Number');
             $table->string('SEARDE', 50)->comment('Article description');
             $table->enum('SESIDE', ['L', 'R'])->comment('Is the product left or right');
             $table->date('SEEXPI')->comment('Delivery expiration date');
             $table->string('SEPILL', 2)->comment('Which pillar of the car (which CSV)');
             $table->boolean('SEINPR')->default(false)->comment('Candidate for production');
+            $table->string('PRCODE', 32)->nullable()->comment('Product code (PRODUCT:PRCODE)');
             $table->string('ORCODE', 32)->nullable()->comment('Order code (ORDERHEAD:ORCODE)');
         });
 
