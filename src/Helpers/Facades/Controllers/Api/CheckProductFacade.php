25462eb975f8cf52f2872fbd887adc1952bd86dc
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-02-16 21:02:53 +0100

    FIX

--- src/Helpers/Facades/Controllers/Api/CheckProductFacade.php
+++ src/Helpers/Facades/Controllers/Api/CheckProductFacade.php
@@ -14,26 +14,50 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 use App\Models\SERIALNUMBER;
 use App\Models\WORKCENTER;
+use App\Models\DASHBOARD_MODULE_SETTINGS;
 use App\Models\REPNO;
 use App\Models\DASHBOARD;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\DB;
 
 class CheckProductFacade extends Facade
 {
     public function check(Request|FormRequest|null $request = null, Model|string|null $model = null): JsonResponse|null
     {
-        $workCenter = WORKCENTER::where(
-            'WCSHNA',
-            '=',
-            DASHBOARD::where('DHIDEN', '=', $request->dashboard)->first()->operatorPanels[0]->WCSHNA
-        )->first();
+        $moduleSetting = DASHBOARD_MODULE_SETTINGS::where([
+            ['DHIDEN', $request->dashboard],
+            ['DMSKEY', 'scrapStationFilter']
+        ])
+            ->first();
+
+        if ($moduleSetting) {
+            $scrapStationFilter = $moduleSetting->DMSVAL;
+            // Ha van beallitva ertek a szuroben
+            $workCenter = WORKCENTER::find($scrapStationFilter); // Selejt állomás megkeresese
+        }
+
+        $start = (new \DateTime())->format('Y-m-d') . ' 00:00:00';
+        $database = config('database.connections.mysql.database');
 
         $sequenceLog = AWF_SEQUENCE_LOG::where('WCSHNA', '=', $workCenter->WCSHNA)
             ->whereNull('LSTIME')
             ->whereNull('LETIME')
             ->first();
 
-        if (empty($sequenceLog)) {
+        $waitings = DB::connection('custom_mysql')->select('
+            select asl.LSTIME, a.SEQUID, a.SEPONR, a.SEPSEQ, a.SESIDE, a.SEPILL, a.SEINPR, a.PRCODE, a.ORCODE, ppd.PFIDEN, ppd.PORANK, ppd.OPSHNA
+            from AWF_SEQUENCE a
+                join ' . $database . '.PRWFDATA pfd on pfd.PRCODE = a.PRCODE
+                join ' . $database . '.PRWCDATA pcd on pfd.PFIDEN = pcd.PFIDEN and pcd.WCSHNA = "' . $workCenter->WCSHNA . '"
+                join ' . $database . '.PROPDATA ppd on ppd.PFIDEN = pcd.PFIDEN and ppd.OPSHNA = pcd.OPSHNA
+                left join AWF_SEQUENCE_LOG asl on a.SEQUID = asl.SEQUID and asl.WCSHNA = pcd.WCSHNA
+                left join AWF_SEQUENCE_WORKCENTER asw on a.SEQUID = asw.SEQUID and asw.WCSHNA = pcd.WCSHNA
+            where a.SEINPR < ppd.PORANK
+                and (asl.LSTIME >= "' . $start . '" or asl.LSTIME is null)
+            order by asl.LSTIME DESC, a.SEQUID limit 1
+        ');
+
+        if (!array_key_exists(0, $waitings) || empty($waitings[0])) {
             return new CustomJsonResponse(new JsonResponseModel(
                 new ResponseData(
                     false,
@@ -44,14 +68,10 @@ class CheckProductFacade extends Facade
             ));
         }
 
-        $sequenceWorkCenter = AWF_SEQUENCE_WORKCENTER::where('WCSHNA', '=', $workCenter->WCSHNA)
-            ->where('SEQUID', '=', $sequenceLog->SEQUID)
-            ->first();
-
         $serial = SERIALNUMBER::where('SNSERN', '=', $request->serial)
             ->first();
 
-        if (empty($serial) || (int)$serial->SNCYID > 0) {
+        if (empty($serial) || (int)$serial->SNCYID < 0) {
             return new CustomJsonResponse(new JsonResponseModel(
                 new ResponseData(
                     false,
@@ -66,7 +86,7 @@ class CheckProductFacade extends Facade
             new ResponseData(
                 true,
                 [
-                    'orderCode' => AWF_SEQUENCE::where('SEQUID', '=', $sequenceWorkCenter->SEQUID)->first()->ORCODE,
+                    'orderCode' => AWF_SEQUENCE::where('SEQUID', '=', $waitings[0]->SEQUID)->first()->ORCODE,
                 ]
             ),
             Response::HTTP_OK
