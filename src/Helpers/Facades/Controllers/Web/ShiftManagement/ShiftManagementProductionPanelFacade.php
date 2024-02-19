ddea99a96ea38d736739aa180cecd60037e28285
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-02-19 13:09:58 +0100

    FIX shift management panel production details page got over and in place sql

--- src/Helpers/Facades/Controllers/Web/ShiftManagement/ShiftManagementProductionPanelFacade.php
+++ src/Helpers/Facades/Controllers/Web/ShiftManagement/ShiftManagementProductionPanelFacade.php
@@ -59,7 +59,7 @@ class ShiftManagementProductionPanelFacade extends Facade
             where asl.LSTIME is not null and asl.LSTIME >= "' . $start . '" and asl.LSTIME >= (
                     select max(LSTIME) from AWF_SEQUENCE_LOG where LSTIME is not null and LETIME is not null and WCSHNA = asl.WCSHNA
                 )
-                and asl.LETIME is not null and a.SEINPR = ppd.PORANK and asl.WCSHNA = pcd.WCSHNA
+                and asl.LETIME is not null and a.SEINPR >= ppd.PORANK and asl.WCSHNA = pcd.WCSHNA
         ');
 
         if (!empty($gotOver[0])) {
@@ -74,7 +74,7 @@ class ShiftManagementProductionPanelFacade extends Facade
                 join ' . $database . '.PRWCDATA pcd on pfd.PFIDEN = pcd.PFIDEN and pcd.WCSHNA = asl.WCSHNA
                 join ' . $database . '.PROPDATA ppd on ppd.PFIDEN = pcd.PFIDEN and ppd.OPSHNA = pcd.OPSHNA
             where a.SEINPR = ppd.PORANK
-                and (asl.LSTIME >= "' . $start . '" or asl.LSTIME is null)
+                and (asl.LSTIME >= "' . $start . '" or asl.LSTIME is null) and asl.LETIME is null
             order by asl.LSTIME DESC, a.SEQUID
         ');
 
