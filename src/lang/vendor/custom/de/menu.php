c22913268de18573dab0677d36545f41ba5bcc3d
kassaikristof <143385844+kassaikristof@users.noreply.github.com>
2024-06-28 08:39:08 +0200

    ADD custom menu language file

---
+++ src/lang/vendor/custom/de/menu.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'ext' => [
+        'awf' => [
+            'porsche' => [
+                'report' => [
+                    'productionDetails' => 'Produktionsdetails',
+                ]
+            ],
+        ],
+    ],
+];
---
+++ src/lang/vendor/custom/en/menu.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'ext' => [
+        'awf' => [
+            'porsche' => [
+                'report' => [
+                    'productionDetails' => 'AWF Production Details',
+                ]
+            ],
+        ],
+    ],
+];
